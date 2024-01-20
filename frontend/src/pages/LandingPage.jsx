6d0a3f531b1cee23a36faca67ecf82ed5d014b95
Victor <victorjzhao@gmail.com>
2024-01-20 14:51:43 -0500

    assets

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,22 @@
+const LandingPage = () => { 
+  return (
+    <div>
+      <title>InfluenSmart: Your Personal Finance Compass</title>
+
+      <body>
+        <h2>
+          Conquer your finances
+        </h2>
+        <p>
+          Receive personalized advice for your important financial decisions.
+        </p>
+        <button>
+          Get Started
+        </button>
+      </body>
+    </div>
+  )
+}
+
+
+export default LandingPage;
