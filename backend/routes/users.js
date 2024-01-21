f987106c2d8f78d8839b31878f8bfaf4b7a98d71
Chen Jun Chi <chen.j.chi@mail.mcgill.ca>
2024-01-20 19:51:52 -0500

    remove comments

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,38 +4,11 @@ const UserController = require('../controllers/Usercontroller')
 
 const router = express.Router()
 
-// // get all users
-// router.get('/', (req, res) => {
-//     res.json({msg: 'Get all users'})
-// })
-
-// // get a user
-// router.get('/:id', (req, res) => {
-//     res.json({msg: 'Get single user'})
-// })
-
-// // create a user
-// router.post('/', async (req, res) => {
-//     const {email, password, lastName, firstName} = req.body;
-//     try {
-//         // Check if a user with the same email already exists
-//         const existingUser = await User.findOne({ email });
-    
-//         if (existingUser) {
-//           // User with the same email already exists, handle accordingly
-//           return res.status(400).json({ message: 'User with this email already exists.' });
-//         }
-//         const user = await User.create({email, password, lastName, firstName})
-//         res.status(200).json(user)
-//     } catch (error) {
-//         res.status(400).json({error: error.message})
-//     }
-// })
-
+// create user
 router.post('/',UserController.createUser)
+// get all users
 router.get('/',UserController.getAllUsers)
+// get a single user
 router.get('/:id',UserController.getUser)
-
-
 
 module.exports = router
